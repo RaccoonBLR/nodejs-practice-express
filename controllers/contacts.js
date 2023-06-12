e93b4c1685fce88f7f7020bd4852ca234439b84b
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-06-12 20:39:53 +0300

    added filtering contacts by favorite property

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,13 +4,17 @@ const Contact = require("../models/contact");
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
 
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite = [true, false] } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  });
+  const result = await Contact.find(
+    { owner, favorite },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  );
   res.json(result);
 };
 
