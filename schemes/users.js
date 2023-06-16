fd5c79fe7ad7b07b19b2d1d264f46e2c82901d93
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-06-16 16:47:11 +0300

    added email validation scheme when resending a verification request

--- schemes/users.js
+++ schemes/users.js
@@ -5,9 +5,15 @@ const registerScheme = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailScheme = Joi.object({
+  email: Joi.string().email().required().messages({
+    "any.required": "missing required field email",
+  }),
+});
+
 const loginScheme = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
 
-module.exports = { registerScheme, loginScheme };
+module.exports = { registerScheme, loginScheme, emailScheme };
